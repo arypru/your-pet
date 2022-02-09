e7f77db7de6a810b01fe8fefd331bb3a5c2ce035
arypru <arapruy@gmail.com>
2022-02-09 12:52:33 -0300

    feat: installation material icons
    installation material icons from google fonts via link

--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -1,5 +1,7 @@
 <template>
-    <router-link exact-active-class="text-red-500 text-4xl font-sansita-regular underline decoration-wavy"  :to="link" class="hover:underline hover:decoration-wavy text-3xl hover:font-bold text-white hover:text-white rounded-md font-sansita-regular"> {{ texto }}</router-link>
+    <router-link exact-active-class="text-red-500 text-4xl font-sansita-regular underline decoration-wavy font-bold"  :to="link" class="hover:underline hover:decoration-wavy text-3xl hover:font-bold text-white hover:text-white rounded-md font-sansita-regular">
+      <span class="material-icons">{{icon}}</span> {{ texto }}
+    </router-link>
 </template>
 
 <script>
@@ -8,6 +10,11 @@ export default {
     texto: {
       type: String,
       default: 'no-text',
+      required: true,
+    },
+
+    icon: {
+      type: String,
       required: true,
     },
 
--- src/layout/TheNavbar.vue
+++ src/layout/TheNavbar.vue
@@ -1,17 +1,17 @@
 <template>
   <nav class="bg-red-500 w-52 h-full
               grid content-center
-              py-6 pl-10
+              py-6 pl-6
               flex flex-col space-y-6  items-stretch
               sm:items-stretch sm:justify-start
               shadow-2xl bg-opacity-30"
        style="backdrop-filter: blur(2px);">
 
-      <navbar-item texto="Inicio" link="/"/>
-      <navbar-item texto="Nosotros" link="/nosotros"/>
-      <navbar-item texto="Mascotas" link="/adoptar"/>
-      <navbar-item texto="Requisitos" link="/requisitos"/>
-      <navbar-item texto="Donar" link="/donar"/>
+    <navbar-item icon="home" texto="Inicio" link="/"/>
+    <navbar-item icon="people_alt" texto="Nosotros" link="/nosotros"/>
+    <navbar-item icon="pets" texto="Mascotas" link="/adoptar"/>
+    <navbar-item icon="text_snippet" texto="Requisitos" link="/requisitos"/>
+    <navbar-item icon="volunteer_activism" texto="Donar" link="/donar"/>
   </nav>
 </template>
 
