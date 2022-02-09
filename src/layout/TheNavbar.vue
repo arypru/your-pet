b76a909f8777fb0a909a84c1dcd5a1dab35d92c9
arypru <arapruy@gmail.com>
2022-02-09 10:25:45 -0300

    feat: add the component navbar

---
+++ src/layout/TheNavbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="bg-red-500 w-48 h-full
+              py-6 pl-10
+              flex flex-col space-y-3 justify-items-center items-stretch
+              sm:items-stretch sm:justify-start
+              shadow-2xl bg-opacity-30"
+       style="backdrop-filter: blur(2px);">
+
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
