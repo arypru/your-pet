a602a960d69d5c9b9cd25826506970cb5a821554
arypru <arapruy@gmail.com>
2022-02-09 11:55:41 -0300

    feat: add navbar item

--- src/layout/TheNavbar.vue
+++ src/layout/TheNavbar.vue
@@ -6,7 +6,20 @@
               shadow-2xl bg-opacity-30"
        style="backdrop-filter: blur(2px);">
 
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
+    <navbar-item texto="Inicio" link="/"/>
+    <navbar-item texto="Nosotros" link="/nosotros"/>
+    <navbar-item texto="Adoptar" link="/adoptar"/>
+    <navbar-item texto="Requisitos" link="/nosotros"/>
+    <navbar-item texto="Donar" link="/nosotros"/>
   </nav>
 </template>
+
+<script>
+import NavbarItem from "../components/NavbarItem"
+
+export default {
+  components: {
+    NavbarItem
+  }
+}
+</script>
