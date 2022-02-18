dff32c48e755eca4051d1b639246e2d4d460a7e5
arypru <arapruy@gmail.com>
2022-02-18 11:57:40 -0300

    feat: make responsive navbar

--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -1,9 +1,13 @@
 <template>
     <router-link
-        exact-active-class="underline underline-offset-8 text-red-400 text-4xl font-sansita-regular font-bold"
-        :to="link"
-        class="hover:text-red-400 text-3xl hover:font-bold text-white font-sansita-regular">
-      <span class="material-icons ">{{icon}}</span> {{ texto }}
+         exact-active-class="text-red-600 lg:text-2xl xl:text-3xl font-sansita-regular font-bold"
+         :to="link"
+         class="hover:text-red-600 hover:font-bold text-white font-sansita-regular lg:text-xl xl:text-2xl">
+         <div class="flex flex-col justify-center items-center
+                      lg:flex-row xl:flex-row 2xl:flex-row lg:justify-start ">
+           <span class="material-icons">{{ icon }}</span>
+           <p>{{ texto }}</p>
+         </div>
     </router-link>
 </template>
 
@@ -29,3 +33,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.material-icons {
+  font-size: 32px;
+}
+</style>
